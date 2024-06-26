109be72106d9d7841225b5ac5dbd3d2c72f8fda2
gui.gutinks@gmail.com <gui.gutinks@gmail.com>
2024-06-26 19:19:04 -0300

    added some setters and verPoltrDisp method

--- src/main/java/Ingresso.java
+++ src/main/java/Ingresso.java
@@ -5,16 +5,40 @@ public class Ingresso{
   private String cpf;
   private String nomeFilme;
   private int numPoltrona;// (de 1 a 120)
-  private float valor;//(16 reais inteira ou 8 reais meia)
-
+  private float valor = 0;//(16 reais inteira ou 8 reais meia)
+  private boolean[] poltronaDisp = new boolean[120];
+  
+  for(int i = 0; i+1 <= 120; i++){
+    poltronaDisp[i] = true;
+  }
 
   public void setCpf(String cpf){
     this.cpf = cpf;
   }
   public void setCod(int cod){
-    this.cod =(int)(1 + Math.random() * 120);
+    this.cod =(int)(1 + Math.random() * 10000);
   }
   public void setNomeFilme(String nomeFilme){
     this.nomeFilme = nomeFilme;
+  }
+  public void setNumPoltrona(int numPoltrona){
+    this.numPoltrona = numPoltrona;
+  }
+  public void setValor(float valor){
+    if(valor == 1){
+      this.valor = 16;
+    } else if(valor == 2){
+      this.valor = 8;
+    }
+  }
+  public void verPoltrDisp() {
+    if(poltronaDisp[numPoltrona] == false){
+          System.out.println("você escolheu uma poltrona já ocupada");
+        } else if(numPoltrona <=0 || numPoltrona >=121){
+          System.out.println("você escolheu uma poltrona inexistente");
+        } else if(poltronaDisp[numPoltrona] == true){
+          System.out.println("você escolheu uma poltrona válida");
+          poltronaDisp[numPoltrona] = true;
+        }
   }
 }
